fd091b1288e13122c5417b368045a7f512df3ddf
calebadavis114 <caleb.a.davis114@gmail.com>
2023-10-08 03:54:58 -0700

    completed

--- bottles.py
+++ bottles.py
@@ -1,7 +1,12 @@
-def bottle_song():
-	# write your code here!
-	pass
+def bottle_song(num):
+	for x in reversed(range(num)):
+		if(x > 1):
+			print(f"{x} bottles of beer on the wall, {x} bottles of beer. Take one down and pass it around,{x - 1} bottles of beer on the wall.")
+			x -= 1
+		elif(x == 1):
+			print("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.")
+			x -= 1
+		else:
+			print("No more bottles of beer on the wall, no more bottles of beer.")
 
-bottle_song()
-
-####kldjsfalkfdajalskdfjdfls
+bottle_song(100)
